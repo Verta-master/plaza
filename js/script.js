8b3ff77713d79f4d3a9dc00ff5d76d230012f977
Verta-master <vertyagina@gmail.com>
2018-12-21 15:43:08 +0600

    Slider

--- js/script.js
+++ js/script.js
@@ -2,69 +2,19 @@ jQuery(function($) {
 	"use strict";
 
 //Mobile menu
-$('.menu__btn').click(function() {
-  $('.menu').toggleClass('menu--opened');
-  $('.menu__list').slideToggle();
-})
-
-if (window.innerWidth < 766) {
-  $('.menu__link').click(function() {
-    $('.menu__list').slideToggle();
-    $('.menu').removeClass('menu--opened');
-  })
-}
-
-//Scroll to top
-$(window).scroll(function(){
-  if ((window.pageYOffset || document.documentElement.scrollTop ||document.body.scrollTop) >= 100) {
-    $(".btn-top").fadeIn();
-  } else {
-    $(".btn-top").fadeOut();
-  };
-});
-
-$(".btn-top").click(function () {
-  $("body,html").animate({
-    scrollTop: 0
-  }, 400);
-  return false;
-});
-
-//Scroll to menu anchor
-$(document).ready(function () {
-  $(document).on("scroll", onScroll);
-
-  //smoothscroll
-  $('.menu__item a[href^="#"]').on('click', function (e) {
-    e.preventDefault();
-    $(document).off("scroll");
-
-    $('.menu__item a').each(function () {
-        $(this).removeClass('menu__link--active');
-    })
-    $(this).addClass('menu__link--active');
-
-    var target = this.hash,
-        menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top
-    }, 500, 'swing', function () {
-        $(document).on("scroll", onScroll);
-    });
+  $('.menu__btn').click(function() {
+    $('.menu__wrap').slideToggle();
   });
-});
-
-function onScroll(event){
-  var scrollPos = $(document).scrollTop();
-  $('.menu__link').each(function () {
-      event.preventDefault();
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-          $('.menu__link').removeClass("menu__link--active");
-          currLink.addClass("menu__link--active");
-      }
+  
+  $('.footer__menu-btn').click(function() {
+    $('.footer__list').slideToggle();
   });
-};
+  
+//scroll to arrow anchors
+  $('.promo__link').click(function(event) {
+    event.preventDefault();
+    var id  = $(this).attr('href');
+    var top = $(id).offset().top;
+    $('body, html').animate({scrollTop: top}, 500);
+  });
 });
